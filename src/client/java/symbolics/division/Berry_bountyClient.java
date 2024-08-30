a7fc188243f62cc2ba6b55e1bf2dfcd2cb2db3c2
eliatlarge <eliatlarge@gmail.com>
2024-08-29 23:02:34 -0700

    initial

---
+++ src/client/java/symbolics/division/Berry_bountyClient.java
@@ -0,0 +1,10 @@
+package symbolics.division;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Berry_bountyClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/symbolics/division/Berry_bountyDataGenerator.java
@@ -0,0 +1,11 @@
+package symbolics.division;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Berry_bountyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
